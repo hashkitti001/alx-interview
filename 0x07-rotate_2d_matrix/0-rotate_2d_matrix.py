d13807afb966afcd137eb20860160449e188fceb
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 14:10:16 +0100

    :wrench: indentation bug fix on 0x07

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,11 +2,11 @@
 """Module for rotate 2D matrix submission."""
 
 def rotate_2d_matrix(matrix):
-    """Rotates a 2D matrix inplace."""
+    """Rotates a 2D matrix in place."""
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
-        for i in range(n):
-            matrix[i].reverse()
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    for row in matrix:
+        row.reverse()
