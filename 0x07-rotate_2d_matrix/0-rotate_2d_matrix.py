ff060b4ae3e4465f82eec442eb808f6db0909b9f
hashkitti001 <iJeddy5.0@gmail.com>
2024-11-20 14:14:11 +0100

    :chore: pycodestyle lint 0x07

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Module for rotate 2D matrix submission."""
 
+
 def rotate_2d_matrix(matrix):
     """Rotates a 2D matrix in place."""
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for row in matrix:
         row.reverse()
